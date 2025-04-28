a530b5ee1eddd177f687360cf2f8ca9d58a79e75
toth-marcell <toth.marcell.21i@egressy.info>
2025-04-28 14:32:19 +0200

    api: add error msgs

--- server.js
+++ server.js
@@ -6,18 +6,36 @@ import { Extinguisher, Inspector } from "./models.js";
 const app = express();
 app.use(express.json());
 
+function APIError(res, msg) {
+  res.status(400).json({ msg: msg });
+}
+
 app.get("/extinguishers", async (req, res) => {
   res.json(await Extinguisher.findAll());
 });
 
 app.post("/extinguishers", async (req, res) => {
-  const newExtinguisher = await Extinguisher.create(req.body);
-  res.json(newExtinguisher);
+  const { materialType, type, capacity, manufactured } = req.body;
+  if (!(materialType && type && capacity && manufactured)) {
+    APIError(res, "Missing fields");
+    return;
+  }
+  const newExtinguisher = await Extinguisher.create({
+    materialType: materialType,
+    type: type,
+    capacity: capacity,
+    manufactured: manufactured,
+  });
+  res.status(201).json(newExtinguisher);
 });
 
 app.put("/extinguishers", async (req, res) => {
   const { id, inspected } = req.body;
   const extinguisher = await Extinguisher.findByPk(id);
+  if (!extinguisher) {
+    APIError(res, "No extinguisher with this id");
+    return;
+  }
   await extinguisher.update({ inspected: inspected });
   res.json(extinguisher);
 });
@@ -25,6 +43,10 @@ app.put("/extinguishers", async (req, res) => {
 app.delete("/extinguishers", async (req, res) => {
   const { id } = req.query;
   const extinguisher = await Extinguisher.findByPk(id);
+  if (!extinguisher) {
+    APIError(res, "No extinguisher with this id");
+    return;
+  }
   await extinguisher.destroy();
   res.end();
 });
